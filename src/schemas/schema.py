60e061dcc232ef6c277183309070703f936d7d9f
Matthew Lee <matthewlee.09213@gmail.com>
2023-03-17 22:16:48 -0400

    added query to get transactions by stock/account/platform (need to move to own file)

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -24,6 +24,8 @@ from schemas.mutations.transaction import (
     DeleteTransactionMutation
 )
 
+from models.models import Transaction
+
 class Mutations(ObjectType):
     create_stock = CreateStockMutation.Field()
     update_stock = UpdateStockMutation.Field()
@@ -42,5 +44,17 @@ class Query(ObjectType):
     stocks = MongoengineConnectionField(StockType)
     transactions = MongoengineConnectionField(TransactionType)
 
+    # TODO: Move these to its own file similar to mutation
+    transactions_by_stock = graphene.List(TransactionType, stockId=graphene.ID())
+    def resolve_transactions_by_stock(self, info, stockId):
+        return Transaction.objects.filter(stock=stockId)
+    
+    transactions_by_account = graphene.List(TransactionType, accountId=graphene.ID())
+    def resolve_transactions_by_account(self, info, accountId):
+        return Transaction.objects.filter(account=accountId)
+    
+    transactions_by_platform = graphene.List(TransactionType, platformId=graphene.ID())
+    def resolve_transactions_by_platform(self, info, platformId):
+        return Transaction.objects.filter(platform=platformId)
 
 schema = graphene.Schema(query = Query, mutation=Mutations, types=[AccountType, ActivityType, CurrencyType, PlatformType, StockType, TransactionType])
